a93e399cb0d146b2029535070b26cef448d2e11d
Stephen Wray <stevedwray@gmail.com>
2021-05-24 12:43:47 +1200

    added profiles

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -1,8 +1,9 @@
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
+# @summary Configures a whole LAMP stack
 #
 # @example
 #   include role::lamp
 class role::lamp {
+  include profile::base
+  include profile::apache
+  include profile::mysql::server
 }
